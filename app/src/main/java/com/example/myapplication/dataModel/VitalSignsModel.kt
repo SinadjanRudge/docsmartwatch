6802c399b2ff96dd776d51ca98ba58c8b7519937
jcano <jcano@csyoutsoutcing.com>
2024-03-20 15:04:28 +0800

    Created VitalSignsModel

---
+++ app/src/main/java/com/example/myapplication/dataModel/VitalSignsModel.kt
@@ -0,0 +1,11 @@
+data class VitalSignsModel(
+    val bloodPressure: String,
+    val temperature: Double,
+    val pulseRate: Int,
+    val oxygenLevel: Int,
+    val weight: Double,
+    val height: Double,
+    val BMI: Double,
+) {
+
+}
