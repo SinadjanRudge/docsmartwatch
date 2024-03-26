8018223279a97c460319e27f5d225013657319d3
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-03-26 12:30:24 +0800

    added Last Sync and can locally saved the Vital Signs data to the device locally using sqlite

--- app/src/main/java/com/triadss/doctrack2/Home_VitalMonitor.kt
+++ app/src/main/java/com/triadss/doctrack2/Home_VitalMonitor.kt
@@ -1,10 +1,10 @@
 package com.triadss.doctrack2
 
-import VitalSignsModel
 import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -15,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.health.services.client.ExerciseClient
-import androidx.health.services.client.ExerciseUpdateCallback
 import androidx.health.services.client.HealthServices
 import androidx.health.services.client.MeasureCallback
 import androidx.health.services.client.MeasureClient
@@ -31,6 +30,7 @@ import com.google.android.gms.wearable.DataEvent
 import com.google.android.gms.wearable.DataMapItem
 import com.google.android.gms.wearable.Node
 import com.google.android.gms.wearable.Wearable
+import com.triadss.doctrack2.dbHelper.VitalSignsDbHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -38,13 +38,13 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONException
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class Home_VitalMonitor : AppCompatActivity() {
     private val CHECK_INTERVAL_MILLISECONDS: Long = 3000
     private val TAG = "VitalMonitor"
     private val PERMISSION_REQUEST_CODE = 1001
-    private lateinit var exerciseClient: ExerciseClient
-    private lateinit var exerciseCallback: ExerciseUpdateCallback
     private lateinit var loadingText: TextView
     private lateinit var heartRateText: TextView
     private lateinit var unavailableVitalsText: TextView
@@ -56,9 +56,9 @@ class Home_VitalMonitor : AppCompatActivity() {
     private lateinit var heightVal: TextView
     private lateinit var bmiVal: TextView
     private lateinit var vitalsContainer: LinearLayout
-    private lateinit var vitalSigns: VitalSignsModel
-    private lateinit var activity: Activity
-    private var checkOnce = false;
+    private var checkOnce = false
+    private lateinit var exerciseClient: ExerciseClient
+    private lateinit var lastSync: TextView
 
     private val dataListener = DataClient.OnDataChangedListener { dataEventBuffer ->
         for (event in dataEventBuffer) {
@@ -80,50 +80,47 @@ class Home_VitalMonitor : AppCompatActivity() {
                     val bmi = jsonObject.getDouble("BMI")
                     val uid = jsonObject.getString("uid")
 
-                    vitalSigns = VitalSignsModel(
-                        bloodPressure,
-                        temperature,
-                        pulseRate,
-                        oxygenLevel,
-                        weight,
-                        height,
-                        bmi
-                    )
+                    val lastSyncMillis = System.currentTimeMillis()
+                    val sdf = SimpleDateFormat("yyyy-MM-dd hh:mm a")
+                    val lastSync = sdf.format(Date(lastSyncMillis))
 
-                    bloodPressureVal.text = bloodPressure
-                    temperatureVal.text = temperature.toString();
-                    pulseRateVal.text = pulseRate.toString()
-                    oxygenLevelVal.text = oxygenLevel.toString()
-                    weightVal.text = weight.toString()
-                    heightVal.text = height.toString()
-                    bmiVal.text = bmi.toString()
+                    Log.e(TAG, "Last sync: $lastSync")
+
+                    // Update UI with received data
+                    updateUI(bloodPressure, temperature, pulseRate, oxygenLevel, weight, height, bmi, lastSync)
+
                 } catch (e: JSONException) {
-                    Log.e(TAG, "Error parsing JSON data: " + e.message)
+                    Log.e(TAG, "Error parsing JSON data: ${e.message}")
                 }
             }
         }
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_vital_monitor)
 
-        activity = this
+        //* Create an instance of your database helper
+//        val dbHelper = VitalSignsDbHelper(this)
+//
+//        //* Get the SQLiteDatabase object by calling writableDatabase
+//        val db = dbHelper.writableDatabase
+
+
+        //* Delete the existing table if it exists
+//        db.execSQL("DROP TABLE IF EXISTS ${MyDatabaseContract.VitalSignsEntry.TABLE_NAME}")
 
         // Initialize views
         initViews()
-
-        val healthClient = HealthServices.getClient(this)
-        val measureClient = healthClient.measureClient
-        exerciseClient = healthClient.exerciseClient
 
         // Check and request permissions if needed
         if (!hasPermissions()) {
             requestPermissions()
         }
-
         initHeartRate()
+
+        // Retrieve data from local storage or database and update UI
+        loadDataAndUpdateUI()
         startContinuousCheck()
     }
 
@@ -138,6 +135,11 @@ class Home_VitalMonitor : AppCompatActivity() {
         heightVal = findViewById(R.id.heightVal)
         bmiVal = findViewById(R.id.bmiVal)
         pulseRateVal = findViewById(R.id.pRateVal)
+        lastSync = findViewById(R.id.lastSyncText)
+
+        val healthClient = HealthServices.getClient(this)
+        val measureClient = healthClient.measureClient
+        exerciseClient = healthClient.exerciseClient
     }
 
     private fun startContinuousCheck() {
@@ -151,101 +153,31 @@ class Home_VitalMonitor : AppCompatActivity() {
 
     private fun checkIfPairedDevice() {
         CoroutineScope(Dispatchers.Main).launch {
-            val nodeListTask: Task<List<Node>> = Wearable.getNodeClient(activity).getConnectedNodes()
+            val nodeListTask: Task<List<Node>> = Wearable.getNodeClient(this@Home_VitalMonitor).connectedNodes
             val nodes: List<Node> = withContext(Dispatchers.IO) {
                 Tasks.await(nodeListTask)
             }
 
-            if (nodes.size == 1) {
-                showPairedDeviceStatus(true)
-            } else {
-                showPairedDeviceStatus(false)
-            }
+            showPairedDeviceStatus(nodes.size == 1)
         }
     }
+
 
     private fun showPairedDeviceStatus(isPaired: Boolean) {
         if (isPaired) {
             vitalsContainer.visibility = View.VISIBLE
             unavailableVitalsText.visibility = View.GONE
             if (!checkOnce) {
-                Toast.makeText(activity, "Paired with a mobile phone", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@Home_VitalMonitor, "Paired with a mobile phone", Toast.LENGTH_SHORT).show()
             }
             checkOnce = true
         } else {
             vitalsContainer.visibility = View.GONE
             unavailableVitalsText.visibility = View.VISIBLE
             if (checkOnce) {
-                Toast.makeText(activity, "Please pair a device with the smartwatch", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@Home_VitalMonitor, "Please pair a device with the smartwatch", Toast.LENGTH_SHORT).show()
                 checkOnce = false
             }
-        }
-    }
-
-    private fun initHeartRate() {
-        val healthClient = HealthServices.getClient(this)
-        val measureClient = healthClient.measureClient
-        exerciseClient = healthClient.exerciseClient
-
-        supportsHeartRate(measureClient, object : EitherCallback {
-            override fun onTrue() {
-                initMeasureClientCallbacks(measureClient)
-            }
-
-            override fun onFalse() {
-                loadingText.visibility = View.GONE
-            }
-        })
-    }
-
-    private fun initMeasureClientCallbacks(measureClient: MeasureClient) {
-        val heartRateCallback = object : MeasureCallback {
-            override fun onAvailabilityChanged(dataType: DeltaDataType<*, *>, availability: Availability) {
-                Log.d(TAG, "Heart rate availability changed: $availability")
-            }
-
-            override fun onDataReceived(data: DataPointContainer) {
-                try {
-                    val heartRateValue = data.sampleDataPoints[0].value
-                    updateHeartRateValue(heartRateValue)
-                } catch (e: Exception) {
-                    Log.e(TAG, "Error receiving heart rate data: ${e.message}")
-                }
-            }
-
-            override fun onRegistered() {
-                super.onRegistered()
-                Log.d(TAG, "Measure client registered")
-            }
-
-            override fun onRegistrationFailed(throwable: Throwable) {
-                super.onRegistrationFailed(throwable)
-                Log.e(TAG, "Measure client registration failed: ${throwable.message}")
-            }
-        }
-
-        measureClient.registerMeasureCallback(
-            DataType.HEART_RATE_BPM,
-            heartRateCallback
-        )
-    }
-
-    private fun supportsHeartRate(measureClient: MeasureClient, eitherCallback: EitherCallback) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val measureCapabilities = measureClient.getCapabilities()
-            withContext(Dispatchers.Main) {
-                when (DataType.HEART_RATE_BPM in measureCapabilities.supportedDataTypesMeasure) {
-                    true -> eitherCallback.onTrue()
-                    false -> eitherCallback.onFalse()
-                }
-            }
-        }
-    }
-
-
-    private fun updateHeartRateValue(newHeartRate: Any) {
-        runOnUiThread {
-            heartRateText.text = "Heart Rate: \n$newHeartRate bpm"
         }
     }
 
@@ -290,8 +222,7 @@ class Home_VitalMonitor : AppCompatActivity() {
             if (grantResults.isNotEmpty() &&
                 grantResults.all { it == PackageManager.PERMISSION_GRANTED }
             ) {
-                Toast.makeText(activity, "All permissions granted", Toast.LENGTH_SHORT).show()
-                initHeartRate()
+                Toast.makeText(this, "All permissions granted", Toast.LENGTH_SHORT).show()
             } else {
                 // Permissions not granted, handle accordingly
 //                Toast.makeText(activity, "All permissions not granted", Toast.LENGTH_SHORT).show()
@@ -301,25 +232,220 @@ class Home_VitalMonitor : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        // Register a listener to receive data events when the activity is resumed
         Wearable.getDataClient(this).addListener(dataListener)
     }
 
     override fun onPause() {
         super.onPause()
-        // Unregister the data listener to avoid memory leaks when the activity is paused
         Wearable.getDataClient(this).removeListener(dataListener)
     }
+
+    private fun updateUI(
+        bloodPressure: String,
+        temperature: Double,
+        pulseRate: Int,
+        oxygenLevel: Int,
+        weight: Double,
+        height: Double,
+        bmi: Double,
+        lastDateSync: String
+    ) {
+        runOnUiThread {
+            bloodPressureVal.text = bloodPressure
+            temperatureVal.text = temperature.toString()
+            pulseRateVal.text = pulseRate.toString()
+            oxygenLevelVal.text = oxygenLevel.toString()
+            weightVal.text = weight.toString()
+            heightVal.text = height.toString()
+            bmiVal.text = bmi.toString()
+            lastSync.text = "Last Sync: $lastDateSync"
+        }
+
+        saveDataLocally(
+            bloodPressure,
+            temperature,
+            pulseRate,
+            oxygenLevel,
+            weight,
+            height,
+            bmi,
+            lastDateSync
+        )
+    }
+
+    private fun saveDataLocally(
+        bloodPressure: String,
+        temperature: Double,
+        pulseRate: Int,
+        oxygenLevel: Int,
+        weight: Double,
+        height: Double,
+        bmi: Double,
+        lastSync: String
+    ) {
+        val dbHelper = VitalSignsDbHelper(this@Home_VitalMonitor)
+        val db = dbHelper.writableDatabase
+
+        val values = ContentValues().apply {
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_BLOOD_PRESSURE, bloodPressure)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_TEMPERATURE, temperature)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_PULSE_RATE, pulseRate)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_OXYGEN_LEVEL, oxygenLevel)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_WEIGHT, weight)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_HEIGHT, height)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_BMI, bmi)
+            put(MyDatabaseContract.VitalSignsEntry.COLUMN_LAST_SYNC, lastSync) // Save last sync time
+        }
+
+        // Insert the data into the database
+        db.insert(MyDatabaseContract.VitalSignsEntry.TABLE_NAME, null, values)
+
+        // Close the database connection
+        db.close()
+    }
+
+
+    private fun initHeartRate() {
+        val healthClient = HealthServices.getClient(this)
+        val measureClient = healthClient.measureClient
+        exerciseClient = healthClient.exerciseClient
+
+        supportsHeartRate(measureClient, object : EitherCallback {
+            override fun onTrue() {
+                initMeasureClientCallbacks(measureClient)
+            }
+
+            override fun onFalse() {
+                loadingText.visibility = View.GONE
+            }
+        })
+    }
+
+
+
+    private fun supportsHeartRate(measureClient: MeasureClient, eitherCallback: EitherCallback) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val measureCapabilities = measureClient.getCapabilities()
+            withContext(Dispatchers.Main) {
+                when (DataType.HEART_RATE_BPM in measureCapabilities.supportedDataTypesMeasure) {
+                    true -> eitherCallback.onTrue()
+                    false -> eitherCallback.onFalse()
+                }
+            }
+        }
+    }
+
+    private fun initMeasureClientCallbacks(measureClient: MeasureClient) {
+        val heartRateCallback = object : MeasureCallback {
+            override fun onAvailabilityChanged(dataType: DeltaDataType<*, *>, availability: Availability) {
+                Log.d(TAG, "Heart rate availability changed: $availability")
+            }
+
+            override fun onDataReceived(data: DataPointContainer) {
+                try {
+                    val heartRateValue = data.sampleDataPoints[0].value
+                    updateHeartRateValue(heartRateValue)
+                } catch (e: Exception) {
+                    Log.e(TAG, "Error receiving heart rate data: ${e.message}")
+                }
+            }
+
+            override fun onRegistered() {
+                super.onRegistered()
+                Log.d(TAG, "Measure client registered")
+            }
+
+            override fun onRegistrationFailed(throwable: Throwable) {
+                super.onRegistrationFailed(throwable)
+                Log.e(TAG, "Measure client registration failed: ${throwable.message}")
+            }
+        }
+
+        measureClient.registerMeasureCallback(
+            DataType.HEART_RATE_BPM,
+            heartRateCallback
+        )
+    }
+    private fun updateHeartRateValue(newHeartRate: Any) {
+        runOnUiThread {
+            heartRateText.text = "Heart Rate: \n$newHeartRate bpm"
+        }
+    }
+
+    private fun loadDataAndUpdateUI() {
+        val dbHelper = VitalSignsDbHelper(this@Home_VitalMonitor)
+        val db = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseContract.VitalSignsEntry.COLUMN_BLOOD_PRESSURE,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_TEMPERATURE,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_PULSE_RATE,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_OXYGEN_LEVEL,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_WEIGHT,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_HEIGHT,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_BMI,
+            MyDatabaseContract.VitalSignsEntry.COLUMN_LAST_SYNC
+        )
+
+        var cursor: Cursor? = null
+
+        try {
+            cursor = db.query(
+                MyDatabaseContract.VitalSignsEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
+
+            cursor?.let { c ->
+                if (c.moveToFirst()) {
+                    val bloodPressureIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_BLOOD_PRESSURE)
+                    val temperatureIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_TEMPERATURE)
+                    val pulseRateIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_PULSE_RATE)
+                    val oxygenLevelIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_OXYGEN_LEVEL)
+                    val weightIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_WEIGHT)
+                    val heightIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_HEIGHT)
+                    val bmiIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_BMI)
+                    val lastSyncIndex = c.getColumnIndexOrThrow(MyDatabaseContract.VitalSignsEntry.COLUMN_LAST_SYNC)
+
+                    val bloodPressure = c.getString(bloodPressureIndex)
+                    val temperature = c.getDouble(temperatureIndex)
+                    val pulseRate = c.getInt(pulseRateIndex)
+                    val oxygenLevel = c.getInt(oxygenLevelIndex)
+                    val weight = c.getDouble(weightIndex)
+                    val height = c.getDouble(heightIndex)
+                    val bmi = c.getDouble(bmiIndex)
+                    val lastSync = c.getString(lastSyncIndex)
+
+                    // Update UI with loaded data
+                    updateUI(bloodPressure, temperature, pulseRate, oxygenLevel, weight, height, bmi, lastSync)
+                } else {
+                    // Handle case where cursor is empty (no data found)
+                    // You can show a message or perform other actions here
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Error loading data from database: ${e.message}")
+            // Handle the error as needed (e.g., show an error message)
+        } finally {
+            cursor?.close()
+            db.close()
+        }
+    }
+
 
     interface EitherCallback {
         fun onTrue()
         fun onFalse()
     }
 
-
     companion object {
         fun Any.log(message: String = "") {
             Log.e("ADAMX", "$this $message")
         }
     }
+
 }
